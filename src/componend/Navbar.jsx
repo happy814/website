9b576eda4f6054cddc12aaa1a433adac8a572fcb
happy814 <rajdey4356@gmail.com>
2024-09-23 22:13:13 +0530

    Navbar  Changes

--- src/componend/Navbar.jsx
+++ src/componend/Navbar.jsx
@@ -4,35 +4,34 @@ import { FaBars, FaTimes } from "react-icons/fa";
 const navLinks = [
   { name: "Home", href: "/" },
   { name: "Donation", href: "/donation" },
-  { name: "Github", href: "/github" },
-  { name: "Discord", href: "/discord" },
+  { name: "Github", href: "https://github.com/Unsigned-Labs" },
+  { name: "Discord", href: "https://discord.gg/YHZU4dwy" },
   { name: "Nostr", href: "/nostr" },
 ];
 
 function Navbar() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false); // Using React's state to control the toggle
-
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen); // Toggling the state on button click
+    setIsMenuOpen(!isMenuOpen);
   };
 
   return (
-    <nav className=" select-none px-7 lg:px-16 py-8 ">
-      <div className="w-full  flex justify-between items-center mx-auto">
+    <nav className="select-none px-7 lg:px-16 py-8 relative ">
+      <div className="w-full flex justify-between items-center mx-auto">
         {/* Logo Section */}
-        <div className="flex items-center">
+        <div className="flex items-center relative z-30 ">
           <a href="/">
             <img className="h-8 w-auto" src="/logo.png" alt="Logo" />
           </a>
         </div>
 
         {/* Menu Links (for larger screens) */}
-        <div className="hidden  md:flex space-x-8 ">
+        <div className="hidden md:flex space-x-8">
           {navLinks.map((link) => (
             <a
               key={link.name}
               href={link.href}
-              className="text-black hover:opacity-80  font-semibold hover:text-gray-900"
+              className="text-black hover:opacity-80 font-semibold hover:text-gray-900"
             >
               {link.name}
             </a>
@@ -40,7 +39,7 @@ function Navbar() {
         </div>
 
         {/* Hamburger Menu Button for Mobile */}
-        <div className="md:hidden">
+        <div className="md:hidden z-30 relative">
           <button
             onClick={toggleMenu} // onClick handler for toggling the menu
             className="text-black"
@@ -56,13 +55,17 @@ function Navbar() {
       </div>
 
       {/* Mobile Menu */}
-      <div className={`md:hidden ${isMenuOpen ? "block" : "hidden"}`}>
-        <div className="flex flex-col absolute w-72 z-20 font-semibold  bg-white mt-4 space-y-2">
+      <div
+        className={`md:hidden absolute  top-0 left-0 w-3/4 h-screen  transition-transform p-5 duration-300 backdrop-blur-md z-20 ${
+          isMenuOpen ? "translate-x-0" : "-translate-x-full"
+        } `}
+      >
+        <div className="flex flex-col space-y-2 mt-20 z-20">
           {navLinks.map((link) => (
             <a
               key={link.name}
               href={link.href}
-              className="block font-semibold px-4 py-2 text-black hover:text-gray-900 hover:opacity-80  "
+              className="block font-semibold text-black hover:text-gray-900 hover:opacity-80 px-4  py-2"
             >
               {link.name}
             </a>
