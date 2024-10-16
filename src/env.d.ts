f26f032446c3f26414317205cc88b44ef9a747bb
happy814 <rajdey4356@gmail.com>
2024-10-16 09:47:32 +0530

    update version

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,12 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+  readonly VITE_API_KEY: string;
+  // Add more variables as needed
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
